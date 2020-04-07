6fb72f73c2d39ba4869d01187b31e9ea5c9b54a3
Kilbane <kilbanen@tcd.ie>
2020-04-07 22:11:11 +0100

    Implemented CompetitionDijkstra class

---
+++ Hilary Term Assignment 2/CompetitionDijkstra.java
@@ -0,0 +1,97 @@
+/*
+ * A Contest to Meet (ACM) is a reality TV contest that sets three contestants at three random
+ * city intersections. In order to win, the three contestants need all to meet at any intersection
+ * of the city as fast as possible.
+ * It should be clear that the contestants may arrive at the intersections at different times, in
+ * which case, the first to arrive can wait until the others arrive.
+ * From an estimated walking speed for each one of the three contestants, ACM wants to determine the
+ * minimum time that a live TV broadcast should last to cover their journey regardless of the contestants’
+ * initial positions and the intersection they finally meet. You are hired to help ACM answer this question.
+ * You may assume the following:
+ *     Each contestant walks at a given estimated speed.
+ *     The city is a collection of intersections in which some pairs are connected by one-way
+ * streets that the contestants can use to traverse the city.
+ *
+ * This class implements the competition using Dijkstra's algorithm
+ */
+import java.util.*;
+import java.io.*;
+
+public class CompetitionDijkstra {
+  int sA, sB, sC;
+  Intersection[] network;
+
+    /**
+     * @param filename: A filename containing the details of the city road network
+     * @param sA, sB, sC: speeds for 3 contestants
+    */
+    CompetitionDijkstra (String filename, int sA, int sB, int sC){
+      this.sA = sA;
+      this.sB = sB;
+      this.sC = sC;
+      
+      File file = new File(filename);
+      try{
+        Scanner scanner = new Scanner(file);
+        int N = scanner.nextInt();
+        int S = scanner.nextInt();
+        this.network = new Intersection[N];
+        
+        for(int i = 0; i < N; i++) {
+          this.network[i] = new Intersection();
+        }
+        
+        Street street;
+        int from;
+        int to;
+        double length;
+        
+        for(int i = 0; i < S; i++) {
+          from = scanner.nextInt();
+          to = scanner.nextInt();
+          length = scanner.nextDouble();
+          street = new Street(from, to, length);
+          this.network[from].addStreet(street);
+        }
+
+        scanner.close();
+      } catch (FileNotFoundException e) {
+        System.err.println("FileNotFoundException caught!");
+        e.printStackTrace();
+      }
+    }
+
+    static class Street {
+      int from, to;
+      double length;
+
+      Street(int from, int to, double length) {
+        this.from = from;
+        this.to = to;
+        this.length = length;
+      }
+    }
+
+    static class Intersection {
+      ArrayList<Street> streets;
+
+      Intersection() {
+        this.streets = new ArrayList<Street>();
+      }
+
+      void addStreet(Street street) {
+        this.streets.add(street);
+      }
+    }
+
+
+    /**
+    * @return int: minimum minutes that will pass before the three contestants can meet
+     */
+    public int timeRequiredforCompetition(){
+
+        //TO DO
+        return -1;
+    }
+
+}
