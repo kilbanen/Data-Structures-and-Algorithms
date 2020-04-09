ba6a99192903bd9cca5e9900e88203c323c1ce97
Kilbane <kilbanen@tcd.ie>
2020-04-09 20:33:27 +0100

    Implemented timeRequiredForCompetition method

--- Hilary Term Assignment 2/CompetitionDijkstra.java
+++ Hilary Term Assignment 2/CompetitionDijkstra.java
@@ -16,10 +16,11 @@
  */
 import java.util.*;
 import java.io.*;
+import java.lang.*;
 
 public class CompetitionDijkstra {
-  int sA, sB, sC;
-  Intersection[] network;
+  int sA, sB, sC, numberOfIntersections, numberOfStreets;
+  Intersection[] city;
 
     /**
      * @param filename: A filename containing the details of the city road network
@@ -33,12 +34,12 @@ public class CompetitionDijkstra {
       File file = new File(filename);
       try{
         Scanner scanner = new Scanner(file);
-        int N = scanner.nextInt();
-        int S = scanner.nextInt();
-        this.network = new Intersection[N];
+        this.numberOfIntersections = scanner.nextInt();
+        this.numberOfStreets = scanner.nextInt();
+        this.city = new Intersection[numberOfIntersections];
         
-        for(int i = 0; i < N; i++) {
-          this.network[i] = new Intersection();
+        for(int i = 0; i < numberOfIntersections; i++) {
+          this.city[i] = new Intersection();
         }
         
         Street street;
@@ -46,12 +47,12 @@ public class CompetitionDijkstra {
         int to;
         double length;
         
-        for(int i = 0; i < S; i++) {
+        for(int i = 0; i < numberOfStreets; i++) {
           from = scanner.nextInt();
           to = scanner.nextInt();
           length = scanner.nextDouble();
           street = new Street(from, to, length);
-          this.network[from].addStreet(street);
+          this.city[from].addStreet(street);
         }
 
         scanner.close();
@@ -61,7 +62,7 @@ public class CompetitionDijkstra {
       }
     }
 
-    static class Street {
+    class Street {
       int from, to;
       double length;
 
@@ -72,26 +73,108 @@ public class CompetitionDijkstra {
       }
     }
 
-    static class Intersection {
+    class Intersection {
       ArrayList<Street> streets;
+      int numberOfStreets;
 
       Intersection() {
         this.streets = new ArrayList<Street>();
+        numberOfStreets = 0;
       }
 
       void addStreet(Street street) {
         this.streets.add(street);
+        numberOfStreets++;
+      }
+      
+      Street getStreet(int index) {
+        return streets.get(index);
       }
     }
 
+    class SPT {
+      int source;
+      double[] distTo;
+      Street[] streetTo;
+      boolean[] intersectionsVisited;
+
+      SPT(int source, Intersection[] city) {
+        this.source = source;
+        int size = city.length;
+        distTo = new double[size];
+        streetTo = new Street[size];
+        intersectionsVisited = new boolean[size];
+
+        for(int i = 0; i < size; i++) {
+          if(i == source)
+            distTo[i] = 0;
+          else
+            distTo[i] = Integer.MAX_VALUE; 
+          intersectionsVisited[i] = false;
+        }
+
+        int closestIntersectionIndex;
+        Intersection closestIntersection;
+        for(int i = 0; i < size; i++) {
+          closestIntersectionIndex = closestIntersectionFromSource();
+          closestIntersection = city[closestIntersectionIndex];
+          for(Street street : closestIntersection.streets) {
+            relaxStreet(street);
+          }
+          intersectionsVisited[closestIntersectionIndex] = true;
+        }
+      }
+
+      void relaxStreet(Street street) {
+        int from = street.from;
+        int to = street.to;
+        if(distTo[to] > distTo[from] + street.length) {
+          distTo[to] = distTo[from] + street.length;
+          streetTo[to] = street;
+        }
+        
+      }
+
+      int closestIntersectionFromSource() {
+        double distance = Integer.MAX_VALUE;
+        int index = -1;
+        for(int i = 0; i < distTo.length; i++) {
+          if(distTo[i] < distance && intersectionsVisited[i] == false) {
+            distance = distTo[i];
+            index = i;
+          }
+        }
+        return index;
+      }
+    }
 
     /**
     * @return int: minimum minutes that will pass before the three contestants can meet
      */
     public int timeRequiredforCompetition(){
+      // The contestants can start at any intersection and choose to meet at any intersection.
+      // Therefore, the worst case scenario is one that requires the slowest contestant to travel the longest path
+      // I need to perform Dijkstra's Shortest Path algorithm for every intersection in the city.
+      // I then need to search through all of the shortest paths and find the longest one
+      // I will then divide the length of the longest shortest path by the slowest walking speed
 
-        //TO DO
-        return -1;
+      SPT[] shortestPathTrees = new SPT[numberOfIntersections];
+      double longestShortestPath = 0;
+      for(int i = 0; i < numberOfIntersections; i++) {
+        shortestPathTrees[i] = new SPT(i, city);
+        for(int j = 0; j < numberOfIntersections; j++) {
+          if(shortestPathTrees[i].distTo[j] > longestShortestPath)
+            longestShortestPath = shortestPathTrees[i].distTo[j];
+        }
+      }
+      int slowestSpeed = sA;
+      if (sB < slowestSpeed) slowestSpeed = sB;
+      if (sC < slowestSpeed) slowestSpeed = sC;
+      double timeRequiredDouble = longestShortestPath * 1000 / slowestSpeed;
+      int timeRequiredInt = (int)Math.ceil(timeRequiredDouble);
+      return timeRequiredInt;
     }
+
+    
 
 }
